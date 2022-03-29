3fbb978f1111cf269be6565581e525d9a0d8fc73
hkocaman1 <hasan.kocaman@ceng.metu.edu.tr>
2022-03-30 00:04:32 +0300

    Explain Single Responsible Principle

---
+++ SRP/src/main/srp/UserPersistenceService.java
@@ -0,0 +1,9 @@
+//A separate class for handling persistence 
+public class UserPersistenceService {
+
+    private Store store = new Store();
+
+    public void saveUser(User user) {
+        store.store(user);
+    }
+}
